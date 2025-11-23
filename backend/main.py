bbaf0fe2969bf05665c5089d99feb1b004ad6cea
ada-tem25 <teo.moncada25@imperial.ac.uk>
2025-11-23 20:16:51 +0000

    main.py amélioré

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,13 @@
+import os
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
 def root():
     return {"status": "ok"}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))  # récupère le port Cloud Run
+    uvicorn.run(app, host="0.0.0.0", port=port)
